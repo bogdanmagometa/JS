fb5c4e0c3c7adda2dcbfe0e7951bad5dd97ae6ad
bogdanmagometa <bogdan.magometa@gmail.com>
2022-05-10 23:10:07 +0300

    Finished Alt + double LMB

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
 let numBoxes = 1;
+let nextBoxNumber = 2;
 let boxContainer = document.getElementsByClassName('box-container')[0];
 
 const LEFT_MOUSE_BUTTON = 1;
 const RIGHT_MOUSE_BUTTON = 3;
 
 function createBox() {
-    ++numBoxes;
-
     let box = document.createElement('div');
     box.classList.add('box');
-    let text = document.createTextNode(numBoxes.toString());
+    let text = document.createTextNode(nextBoxNumber.toString());
     box.appendChild(text);
+
+    ++nextBoxNumber;
 
     return box;
 }
 
 function adjustBox(box) {
-    let rightClicked = false;
     function mouseDownHandler(event) {
-        event.preventDefault();
         if (event.which === LEFT_MOUSE_BUTTON) {
-            if (event.shiftKey) {
-                box.classList.toggle('box-large');
-            }
-
             let offsetX = event.clientX - parseInt(window.getComputedStyle(box).left, 10);
             let offsetY = event.clientY - parseInt(window.getComputedStyle(box).top, 10);
         
@@ -45,34 +40,34 @@ function adjustBox(box) {
     
             boxContainer.addEventListener('mouseup', mouseUpHandler);
             boxContainer.addEventListener('mousemove', mouseMoveHandler);    
-        } else if (event.which === RIGHT_MOUSE_BUTTON && !event.shiftKey) {
-            if(rightClicked) {
-                clearTimeout(timeout);
-                rightClicked = false;
-                let randomColor = Math.floor(Math.random() * 2**(8*3)).toString(16);
-                box.style.backgroundColor = '#' + randomColor.toString();
-            } else {
-                rightClicked = true;
-                timeout = setTimeout( function() {
-                    rightClicked = false;
-                }, 300);
-            }  
         }
     }
 
     box.addEventListener('mousedown', mouseDownHandler);
     box.addEventListener('contextmenu', event => {
         event.preventDefault();
+        if (event.which === RIGHT_MOUSE_BUTTON) {
+            let randomColor = Math.floor(Math.random() * 2**(8*3)).toString(16);
+            box.style.backgroundColor = '#' + randomColor.toString();
+        }
     });
-    // box.addEventListener('dblclick'), event => {
-    //     if (event.which === LEFT_MOUSE_BUTTON && event.altKey) {
-    //         box.remove();
-    //     }
-    // }
+    box.addEventListener('click', event => {
+        if (event.which === LEFT_MOUSE_BUTTON && event.shiftKey) {
+            box.classList.toggle('box-large');
+            return;
+        }
+    });
+    box.addEventListener('dblclick', event => {
+        if (event.which === LEFT_MOUSE_BUTTON && event.altKey && numBoxes > 1) {
+            --numBoxes;
+            box.remove();
+        }
+    });
 }
 
 boxContainer.addEventListener('dblclick', event => {
     if (event.which === LEFT_MOUSE_BUTTON && event.target == boxContainer) {
+        ++numBoxes;
         let box = createBox();
         adjustBox(box);
         boxContainer.appendChild(box);
